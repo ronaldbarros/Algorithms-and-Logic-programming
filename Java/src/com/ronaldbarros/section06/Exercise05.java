2663fc9b4c6b58132fd7d3c37a8a24e5386fe3cf
ronaldbarros <rsmbarros@gmail.com>
2021-05-15 22:47:40 -0300

    update section 06

---
+++ Java/src/com/ronaldbarros/section06/Exercise05.java
@@ -0,0 +1,28 @@
+package com.ronaldbarros.section06;
+
+import java.util.Scanner;
+
+public class Exercise05 {
+	
+	public static void main(String[] args) {
+		
+		float weight, overweight, fee;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Fish's weight: ");
+		weight = keyboard.nextFloat();
+		
+		if(weight > 50) {
+			overweight = weight - 50;
+			fee = overweight * 4;
+			System.out.println("Weight: " + weight);
+			System.out.println("Overweight: " + overweight);
+			System.out.println("Fee: US$ " + fee);
+		}else {
+			System.out.println("Weight: " + weight);
+		}
+		keyboard.close();
+	}
+
+}
