7caff43601e5df518d4c89017f01083c9d4d7583
Ronald Barros <rsmbarros@gmail.com>
2021-05-28 00:10:15 -0300

    create section08 exercise03

---
+++ Java/src/com/ronaldbarros/section08/arrays/Exercise03.java
@@ -0,0 +1,23 @@
+package com.ronaldbarros.section08.arrays;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+
+        int [] vector = new int[10];
+
+        Scanner input = new Scanner(System.in);
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("Enter a value:");
+            vector[i] = input.nextInt();
+        }
+
+        for(int i = vector.length - 1; i >= 0; i--){
+            System.out.printf("%d ", vector[i]);
+        }
+
+        input.close();
+    }
+}
